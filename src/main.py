5d97479613cbcb41bbd428d41a83e49f63e6022b
Daniel Vaz <djrv335577@gmail.com>
2018-11-24 14:17:23 +0100

    exp

--- src/main.py
+++ src/main.py
@@ -4,14 +4,28 @@
 """
 
 import os
-from os.path import join
-import cv2
-from paths import data_path
 import ipdb
+from os.path import join
+
+import numpy as np
+import cv2
+
+from paths import data_path
 
 
 if __name__ == "__main__":
-
     img = cv2.imread(join(data_path, '2017-01-20 - Lidl.png'), cv2.IMREAD_GRAYSCALE)
+
+    kernel  = np.ones((3,3),np.uint8)
+    kernel[0,2] = kernel[2,2] = kernel[0,0] = kernel[2,0] = 0
+    img2 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    img2 = cv2.morphologyEx(img2, cv2.MORPH_OPEN, kernel)
+    # img2 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+    cv2.imshow('dst',img2)
+    # cv2.imwrite(filename + ".jpg", img)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+
     print img
     print 'Eureka!'
--- src/paths.py
+++ src/paths.py
@@ -8,8 +8,12 @@ import socket
 def get_machine():
     if socket.gethostname() == 'mbk-12-72':
         return 'alina'
-    else:
+    elif socket.gethostname() == 'VAZUS':
         return 'daniel'
+    else:
+        return ""
 
 if get_machine() == 'alina':
     data_path = '/Users/alina/Documents/code/lauzhack/data/input'
+elif get_machine() == 'daniel':
+    data_path = '../../local-data/receipts'
